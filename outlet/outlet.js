96a43d31a1b178937bdcdee945417d05ec79e1c9
Amit Dethe <26amitdethe@gmail.com>
2020-09-28 11:56:09 +0530

    Quantity input changed

--- outlet/outlet.js
+++ outlet/outlet.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(session({
-  secret: "Our little secret.",
+  secret: "Secret Key",
   resave: false,
   saveUninitialized: false
 }));
@@ -29,7 +29,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb+srv://admin-amit:sonunandini@cluster0.k902j.mongodb.net/FoodEx", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/FoodEx", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("useCreateIndex", true);
 mongoose.set('useFindAndModify', false);
 
